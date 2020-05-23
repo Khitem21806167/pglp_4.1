e43f5623cd5d44d9c8884db82db0ef655deddc9b
KHAMARI Nor El Khitem <61509662+Khitem21806167@users.noreply.github.com>
2020-05-23 14:50:26 +0200

    les deux types d'affichage (mnv package exec:java) pour l'exe

--- pglp4_1/src/main/java/Exo/pglp4_1/ImmuablePersonnel.java
+++ pglp4_1/src/main/java/Exo/pglp4_1/ImmuablePersonnel.java
@@ -1,8 +1,11 @@
 package Exo.pglp4_1;
 import java.time.LocalDate;
+
 import java.time.format.DateTimeFormatter;
 
 import Exo.pglp4_1.Personnel.Builder;
+
+//pour l'execution mvn package exec:java
 
 public enum ImmuablePersonnel {
 ENVIRONNEMENT;
@@ -12,8 +15,10 @@ ENVIRONNEMENT;
 		Builder pb = new Builder("khamari", "nor el khitem")
 				.fonctions("etudiante")
 				.fonctions("agent de production")
+				.fonctions("agent de tri")
 				.dateNais(1997,4,11)
-				.numtel(new TypeTelephone("fax","05521852"))
+				.numtel(new TypeTelephone("fix1","05521852"))
+				.numtel(new TypeTelephone("fix2","014785632"))
 				.numtel(new TypeTelephone("Fax", "0612345678"));
 		
 		Personnel p1 = pb.build(),
@@ -37,9 +42,23 @@ ENVIRONNEMENT;
 		return pg1;
 	}	
 	
+	private void afficherParcours(TypePersonnalIterator pti) {
+		System.out.println("-" + pti.getRacine().typeString());
+		while (pti.hasNext()) {
+			TypePersonnel personnelType = (TypePersonnel) pti.next();
+			System.out.println("-" + personnelType.typeString());
+		}
+	}
+	
 	private void run(String[] args) {
-		GroupePersonnel pg = init();	
-			
+		GroupePersonnel pg = init();
+		
+		System.out.println("Strategie en utilisant Vecteur : ");
+		afficherParcours(new StrategieVecteur(pg));
+		
+		System.out.println("Strategie en utilisant unePile : ");
+		afficherParcours(new StrategiePile(pg));
+
 	}
 	
 	public static void main(String[] args) {
